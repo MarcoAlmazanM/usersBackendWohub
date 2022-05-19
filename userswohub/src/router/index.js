be37016e0cca0b18b1ed5c2e33559f07d3f6c702
Marco Almazan <marco.almazan@outlook.com>
2022-05-19 01:15:26 -0500

    Delete unuseful files

--- userswohub/src/router/index.js
+++ userswohub/src/router/index.js
@@ -13,14 +13,7 @@ const routes = [
       authRequired: true,
     },
   },
-  {
-    path: '/courses',
-    name: 'Courses',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "Courses" */ '../views/Courses.vue')
-  },
+
   {
     path: '/login',
     name: 'LogIn',
@@ -30,11 +23,6 @@ const routes = [
     path: '/register',
     name: 'Register',
     component: () => import(/* webpackChunkName: "Register" */ '../views/Register.vue')
-  },
-  {
-    path: '/resume',
-    name: 'Resume',
-    component: () => import(/* webpackChunkName: "Resume" */ '../views/Resume.vue')
   },
   {
     path: '/home',
@@ -49,16 +37,6 @@ const routes = [
     name: 'SurveyView',
     component: ()=> import(/* webpackChunkName: "SurveyView" */ '../views/SurveyView.vue')
   },
-  {
-    path: '/personal',
-    name: 'PersonalScreen',
-    component: ()=> import(/* webpackChunkName: "PersonalScreen" */ '../views/PersonalScreen.vue')
-  },
-  {
-    path: '/careerawards',
-    name: 'CareerAwardsView',
-    component: ()=> import(/* webpackChunkName: "CareerAwardsView" */ '../views/CareerAwardsView.vue')
-  }
 ]
 
 const router = new VueRouter({
