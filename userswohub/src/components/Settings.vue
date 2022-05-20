36a814bbb5d1eca2b0d76b1256df24a14d8a0e02
ANVRRT <albertonavarreteramirez@gmail.com>
2022-05-19 20:42:52 -0500

    Added Your Goals view

--- userswohub/src/components/Settings.vue
+++ userswohub/src/components/Settings.vue
@@ -3,7 +3,7 @@
     <v-spacer></v-spacer>
     <router-link style="text-decoration: none; " to="/personal"><p class="whiteTec--text text-h6 pa-1 mb-1">Personal Info</p></router-link>
     <v-spacer></v-spacer>
-    <router-link style="text-decoration: none; " to="/careerawards"><p class="whiteTec--text text-h6 pa-1 mb-1">Career Awards</p></router-link>
+    <router-link style="text-decoration: none; " to="/goals"><p class="whiteTec--text text-h6 pa-1 mb-1">Your Goals</p></router-link>
     <v-spacer></v-spacer>
     <router-link style="text-decoration: none; " to="/resume"><p class="whiteTec--text pa-1 mb-1 text-h6">Resume</p></router-link>
     <v-spacer></v-spacer>
---
+++ userswohub/src/views/InterestsView.vue
@@ -0,0 +1,18 @@
+<template>
+  <Interests></Interests>
+</template>
+
+<script>
+import Interests from "@/components/Interests";
+
+export default {
+  name: "InterestsView",
+  components:{
+    Interests,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
